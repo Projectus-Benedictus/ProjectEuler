f8179607fc0f58e9c764ebc1e7908f77436c4f42
JoaoCalhau <joaocalhau6@live.com.pt>
2014-09-24 21:46:12 +0100

    Second Problem tested and submited. Working fine!

--- ProjectEuler/src/Euler.java
+++ ProjectEuler/src/Euler.java
@@ -1,3 +1,5 @@
+import java.math.*;
+
 public class Euler {
 	
 	public Euler(){
@@ -6,14 +8,23 @@ public class Euler {
 	public int Problem1(){
 		int sum=0;
 		for(int i=3;i<1000;i++){
-			if(i%3==0 || i%5==0){
-				sum+=i;
+			if(i%3==0 || i%5==0){sum+=i;
 			}
 		}return sum;
 	}
 	
 	
 	public int Problem2(){
-		
+		int first=0;
+		int second=1;
+		int result=0;
+		int sum=0;
+		while (result<4000000){
+			if (result%2==0){sum+=result;
+			}
+			first=second;
+			second=result;
+			result=first+second;
+		}return sum;
 	}
 }
--- ProjectEuler/src/TestClass.java
+++ ProjectEuler/src/TestClass.java
@@ -3,7 +3,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		
 		Euler cenas = new Euler();
-		System.out.println(cenas.Problem1());
+		System.out.println("Problema 1: "+cenas.Problem1());
+		System.out.println("Problema 2: "+cenas.Problem2());
 	}
-
 }
