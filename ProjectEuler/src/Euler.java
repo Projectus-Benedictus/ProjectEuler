040639b4f2dc003fbb3c0dc5d7d7bd0fcb390070
JoaoCalhau <joaocalhau6@live.com.pt>
2014-09-24 16:54:18 +0100

    ProjectEuler(Calhau)

---
+++ ProjectEuler/src/Euler.java
@@ -0,0 +1,14 @@
+public class Euler {
+	
+	public Euler(){
+	}
+	
+	public int Problem1(){
+		int sum=0;
+		for(int i=3;i<1000;i++){
+			if(i%3==0 || i%5==0){
+				sum+=i;
+			}
+		}return sum;
+	}
+}
---
+++ ProjectEuler/src/TestClass.java
@@ -0,0 +1,9 @@
+
+public class TestClass {
+	public static void main(String[] args) {
+		
+		Euler cenas = new Euler();
+		System.out.println(cenas.Problem1());
+	}
+
+}
