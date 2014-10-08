402a6a36e8e987f2408fab185a067b29f284cfe7
JoaoCalhau <joaocalhau6@live.com.pt>
2014-10-08 14:12:13 +0100

    Cenas

--- ProjectEuler/src/TestClass_J.java
+++ ProjectEuler/src/TestClass_J.java
@@ -2,11 +2,13 @@ public class TestClass_J {
 	public static void main(String[] args) {
 		
 		Euler_J cenas = new Euler_J();
-		System.out.println("Problema 1: "+cenas.Problem1());
-		System.out.println("Problema 2: "+cenas.Problem2());
-		System.out.println("Problema 3: "+cenas.Problem3());
-		System.out.println("Problema 4: "+cenas.Problem4());
-		System.out.println("Problema 5: "+cenas.Problem5());
-		System.out.println("Problema 6: "+cenas.Problem6(100));
+		//System.out.println("Problema 1: "+cenas.Problem1());
+		//System.out.println("Problema 2: "+cenas.Problem2());
+		//System.out.println("Problema 3: "+cenas.Problem3());
+		//System.out.println("Problema 4: "+cenas.Problem4());
+		//System.out.println("Problema 5: "+cenas.Problem5());
+		//System.out.println("Problema 6: "+cenas.Problem6(100));
+		//System.out.println("Problema 7: "+cenas.Problem7());
+		System.out.println("Problema 8: "+cenas.Problem8());
 	}
 }
