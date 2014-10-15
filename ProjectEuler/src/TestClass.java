a90f1ccc04c11b9e4513d36abd0ed2a0811dd76c
JoaoCalhau <joaocalhau6@live.com.pt>
2014-10-15 11:19:07 +0100

    Update da Classe de Teste

--- ProjectEuler/src/TestClass.java
+++ ProjectEuler/src/TestClass.java
@@ -1,21 +1,21 @@
 public class TestClass {
 	public static void main(String[] args) {
 		
-		Euler cenas = new Euler();
-		System.out.println("Problema 1: "+cenas.Problem1());
-		System.out.println("Problema 2: "+cenas.Problem2());
-		System.out.println("Problema 3: "+cenas.Problem3());
-		System.out.println("Problema 4: "+cenas.Problem4());
-		System.out.println("Problema 5: "+cenas.Problem5());
-		System.out.println("Problema 6: "+cenas.Problem6(100));
-		System.out.println("Problema 7: "+cenas.Problem7());
-		System.out.println("Problema 8: "+cenas.Problem8());
-		System.out.println("Problema 9: "+cenas.Problem9());
-		System.out.println("Problema 10: "+cenas.Problem10());
-		System.out.println("Problema 11: "+cenas.Problem11());
-		System.out.println("Problema 12: "+cenas.Problem12());
-		System.out.println("Problema 13: "+cenas.Problem13());
-		System.out.println("Problema 16: "+cenas.Problem16());
-		System.out.println("Problema 20: "+cenas.Problem20());
+		Euler solved = new Euler();
+		System.out.println("Problema 1: "+solved.Problem1());
+		System.out.println("Problema 2: "+solved.Problem2());
+		System.out.println("Problema 3: "+solved.Problem3());
+		System.out.println("Problema 4: "+solved.Problem4());
+		System.out.println("Problema 5: "+solved.Problem5());
+		System.out.println("Problema 6: "+solved.Problem6(100));
+		System.out.println("Problema 7: "+solved.Problem7());
+		System.out.println("Problema 8: "+solved.Problem8());
+		System.out.println("Problema 9: "+solved.Problem9());
+		System.out.println("Problema 10: "+solved.Problem10());
+		System.out.println("Problema 11: "+solved.Problem11());
+		System.out.println("Problema 12: "+solved.Problem12());
+		System.out.println("Problema 13: "+solved.Problem13());
+		System.out.println("Problema 16: "+solved.Problem16());
+		System.out.println("Problema 20: "+solved.Problem20());
 	}
 }
